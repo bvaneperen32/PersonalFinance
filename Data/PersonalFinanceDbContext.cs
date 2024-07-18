2a8eed44f36a220dc949a5a3181eb06663675336
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-07-18 10:12:44 -0500

    Add project files.

---
+++ Data/PersonalFinanceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinance.Models;
+
+namespace PersonalFinance.Data
+{
+    public class PersonalFinanceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public PersonalFinanceDbContext(DbContextOptions<PersonalFinanceDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonalFinance.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
