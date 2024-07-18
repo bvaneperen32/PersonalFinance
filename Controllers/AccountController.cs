22d3a0a048d3e245be21963cbe8e43b6a136f7aa
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-07-18 12:17:51 -0500

    working reg

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PersonalFinance.Models;
+
+namespace PersonalFinance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+           _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return Ok(new { message = "User registered successfully" });
+                }
+
+                return BadRequest(result.Errors);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+    }
+}
