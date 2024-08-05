ca473f734e3d6793ae730ccedaed3d41b5df0e33
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-08-05 13:02:07 -0500

    sass gulp

---
+++ FrontEndDev/reactapp/webpack.config.js
@@ -0,0 +1,33 @@
+﻿const path = require('path');
+module.exports = {
+    mode: 'development',
+    entry: {
+        RegisterForm: './components/register.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]Compiled.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+    },
+};
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿const { src, dest, series, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styleSite() {
+    return src('./FrontEndDev/styles/**/*.scss')
+        .pipe(sass({ includePaths: './FrontEndDev/styles' }).on('error', sass.logError))
+        .pipe(dest('./wwwroot/css/compiledscss'));
+}
+
+function watchFiles() {
+    watch('./FrontEndDev/styles/**/*.scss', series(styleSite));
+}
+
+exports.watch = parallel(styleSite, watchFiles);
